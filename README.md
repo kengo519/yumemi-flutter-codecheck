f4aa02825337b5569b59a049fbe9b56d8085c53a
kengo519 <kenjey.unity@gmail.com>
2023-09-18 13:59:53 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# yumemi_flutter_codecheck
+
+勉強用に「[株式会社ゆめみ Flutter エンジニアコードチェック課題](https://github.com/yumemi-inc/flutter-engineer-codecheck)」に取り組んだレポジトリ
---
+++ android/app/src/main/kotlin/com/example/yumemi_flutter_codecheck/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yumemi_flutter_codecheck
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
